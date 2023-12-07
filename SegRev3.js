62b303d7eea473c323f63d05949b381cd45ac1ca
GabrielScachePrudencio <bielscache@gmail.com>
2023-12-07 18:11:19 -0300

    initial commit

---
+++ SegRev3.js
@@ -0,0 +1,42 @@
+
+// alert((100*10) + " dolares ")
+$x = 10
+$y = 2
+//alert("o resultado de x + y é igual " + ($x + $y))
+
+
+let numero1 = 1000
+const numeroFixo = 10  // numero que nao pode ser mudado
+
+//alert(numero1)
+//alert(numeroFixo)
+
+
+// function
+function ohayo() {
+    alert("bom dia")
+}
+ohayo()
+
+function soma(a, b) {
+    return a + b
+}
+let resultado = soma(10, 10)
+alert(resultado)
+
+
+
+// objeto
+const celular = {
+    cor: "preto",
+    ligar: function(){
+        alert("ligando")
+    }
+
+}
+
+celular.ligar()
+
+
+
+
